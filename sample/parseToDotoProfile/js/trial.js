dff396d6a8638988a3cd8b4502a66913ab30b393
brmayes <brmayes@live.unc.edu>
2015-11-06 14:04:15 -0500

    starting over on parse extend

--- sample/parseToDotoProfile/js/trial.js
+++ sample/parseToDotoProfile/js/trial.js
@@ -4,122 +4,171 @@
 
 $(function() {
 
-  Parse.$ = jQuery;
+      Parse.$ = jQuery;
 
-  // Initialize Parse with your Parse application javascript keys
-  Parse.initialize("QsLB4iUTW6GmgiVumYJ39cajTFnanijYdodplnGI",
-                   "R3JOMH0D0iE9gLIDEdFuOjdmALTO7KpOD1MBGzRT");
-
-
-  var userAttribute = Parse.Object.extend("userAttribute", {
-
-    defaults: function() {
-      return {
-        content: "empty attribute"
-        //,empty: true
-      };
-    }
-
-  });
+      // Initialize Parse with your Parse application javascript keys
+      Parse.initialize("QsLB4iUTW6GmgiVumYJ39cajTFnanijYdodplnGI",
+        "R3JOMH0D0iE9gLIDEdFuOjdmALTO7KpOD1MBGzRT");
 
 
-  var userAttributeList = Parse.Collection.extend({
+      var userAttribute = Parse.Object.extend("userAttribute", {
 
-    model: userAttribute,
+        defaults: function() {
+          return {
+            content: "empty attribute"
+              //,empty: true
+          };
+        }
 
-    // empty: funtion() {
-    //   return "attribute empty";
-    //   //also need to change empty:false when the user fills in value
-    // }
+      });
 
-  });
+      // This is the transient application state, not persisted on Parse
+      var AppState = Parse.Object.extend("AppState", {
+        defaults: {
+          filter: "all"
+        }
+      });
 
-  var userAttributes = new userAttributeList;
+      var userAttributeList = Parse.Collection.extend({
 
-  var attributeView = Parse.View.extend({
+        model: userAttribute,
 
-    //do I need a tag name?
-    tarName: "li",
+        // empty: funtion() {
+        //   return "attribute empty";
+        //   //also need to change empty:false when the user fills in value
+        // }
 
-    template: _.template($('#item-template').html()),
+      });
 
-    events: {
-      "dblclick label.attribute-content" : "edit",
-      //"click .attribute-destroy"   : "clear", //may not need this either
-      "keypress .edit"      : "updateOnEnter",
-      "blur .edit"          : "close"
-    },
+      var userAttributes = new userAttributeList;
 
-    initialize: function() {
-      _.bindAll(this, 'render', 'close', 'remove');
-      this.model.bind('change', this.render);
-      this.model.bind('destroy', this.remove);
-    },
+      var attributeView = Parse.View.extend({
 
-    render: function() {
-      $(this.el).html(this.template(this.model.toJSON()));
-      this.input = this.$('.edit');
-      return this;
-    },
+        //do I need a tag name?
+        tarName: "li",
 
-    edit: function() {
-      $(this.el).addClass("editing");
-      this.input.focus();
-    },
+        template: _.template($('#item-template').html()),
 
-    close: function() {
-      this.model.save({content: this.input.val()});
-      $(this.el).removeClass("editing");
-    },
+        events: {
+          "dblclick label.attribute-content": "edit",
+          //"click .attribute-destroy"   : "clear", //may not need this either
+          "keypress .edit": "updateOnEnter",
+          "blur .edit": "close"
+        },
 
-    // If you hit `enter`, we're through editing the item.
-    updateOnEnter: function(e) {
-      if (e.keyCode == 13) this.close();
-    }
+        initialize: function() {
+          _.bindAll(this, 'render', 'close', 'remove');
+          this.model.bind('change', this.render);
+          this.model.bind('destroy', this.remove);
+        },
 
-  });
+        render: function() {
+          $(this.el).html(this.template(this.model.toJSON()));
+          this.input = this.$('.edit');
+          return this;
+        },
 
-  // The Application
-  // ---------------
+        edit: function() {
+          $(this.el).addClass("editing");
+          this.input.focus();
+        },
 
-  // The main view that lets a user manage their todo items
-  var ManageUserAttributes = Parse.View.extend({
+        close: function() {
+          this.model.save({
+            content: this.input.val()
+          });
+          $(this.el).removeClass("editing");
+        },
 
-    events: {
-      "keypress #new-attribute":  "createOnEnter"
-    },
+        // If you hit `enter`, we're through editing the item.
+        updateOnEnter: function(e) {
+          if (e.keyCode == 13) this.close();
+        }
 
-    el: ".content",
+      });
 
-    // At initialization we bind to the relevant events on the `Todos`
-    // collection, when items are added or changed. Kick things off by
-    // loading any preexisting todos that might be saved to Parse.
-    initialize: function() {
-      var self = this;
+      // The Application
+      // ---------------
 
-      _.bindAll(this, 'addOne', 'addAll', 'addSome', 'render', 'createOnEnter');
+      // The main view that lets a user manage their todo items
+      var ManageUserAttributes = Parse.View.extend({
 
-      // Main todo management template
-      this.$el.html(_.template($("#manage-user-attributes").html()));
+          events: {
+            "keypress #new-attribute": "createOnEnter"
+          },
 
-      this.input = this.$("#new-attribute");
+          el: ".content",
 
-      // Create our collection of attributes
-      this.userAttributes = new userAttributeList;
+          // At initialization we bind to the relevant events on the `Todos`
+          // collection, when items are added or changed. Kick things off by
+          // loading any preexisting todos that might be saved to Parse.
+          initialize: function() {
+            var self = this;
 
-      // Setup the query for the collection to look for attributes from the current user
-      this.userAttributes.query = new Parse.Query(Todo);
-      this.userAttributes.query.equalTo("user", Parse.User.current());
+            _.bindAll(this, 'addOne', 'addAll', 'addSome', 'render', 'createOnEnter');
 
-      this.userAttributes.bind('add',     this.addOne);
-      this.userAttributes.bind('all',     this.render);
+            // Main todo management template
+            this.$el.html(_.template($("#manage-userAttributes-template").html()));
 
-      // Fetch all the todo items for this user
-      this.userAttributes.fetch();
+            this.input = this.$("#new-attribute");
 
-      state.on("change", this.filter, this);
-    },
+            // Create our collection of attributes
+            this.userAttributes = new userAttributeList;
 
-  });
+            // Setup the query for the collection to look for attributes from the current user
+            this.userAttributes.query = new Parse.Query(Todo);
+            this.userAttributes.query.equalTo("user", Parse.User.current());
 
-});
+            this.userAttributes.bind('add', this.addOne);
+            this.userAttributes.bind('all', this.render);
+
+            // Fetch all the todo items for this user
+            this.userAttributes.fetch();
+
+            state.on("change", this.filter, this);
+          },
+
+          // Add a single todo item to the list by creating a view for it, and
+          // appending its element to the `<ul>`.
+          addOne: function(todo) {
+            var view = new TodoView({
+              model: todo
+            });
+            this.$("#todo-list").append(view.render().el);
+          },
+
+          // If you hit return in the main input field, create new Todo model
+          createOnEnter: function(e) {
+            var self = this;
+            if (e.keyCode != 13) return;
+
+            this.todos.create({
+              content: this.input.val(),
+              //order:   this.userAttributes.nextOrder(),
+              //done:    false,
+              user: Parse.User.current(),
+              ACL: new Parse.ACL(Parse.User.current())
+            });
+
+            this.input.val('');
+
+          }
+        });
+
+        // The main view for the app
+        var AppView = Parse.View.extend({
+          // Instead of generating a new element, bind to the existing skeleton of
+          // the App already present in the HTML.
+          el: $("#userapp"),
+
+          initialize: function() {
+            this.render();
+          }
+
+        });
+
+        var state = new AppState;
+
+        new AppView; 
+
+      });
