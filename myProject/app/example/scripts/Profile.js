c85528cfd12b2325cd7f1001ee80a6afb9fe1fd1
brmayes <brmayes@live.unc.edu>
2015-11-27 08:33:21 -0500

    still formatting profile page, finally

--- myProject/app/example/scripts/Profile.js
+++ myProject/app/example/scripts/Profile.js
@@ -27,6 +27,19 @@ angular
     var twitter;
     var instagram;
     var roleTeam;
+    var hometownLabel;
+    var classStatusLabel;
+    var primaryMajorLabel;
+    var doubleMajorLabel;
+    var primaryMinorLabel;
+    var doubleMinorLabel;
+    var academicInterestsLabel;
+    var campusInvolvementLabel;
+    var phoneNumLabel;
+    var personalEmailLabel;
+    var facebookURLLabel;
+    var twitterURLLabel;
+    var instagramURLLabel;
 
     document.getElementById("profilePicture").innerHTML = profilePicture;
 
@@ -44,47 +57,64 @@ angular
         teamNum = user.get("userTeamNum");
           roleTeam = role + " - Team " + teamNum;
         classStatus = user.get("userClassStatus");
+          classStatusLabel = "Class status: " + classStatus;
         bio = user.get("userBio");
         hometown = user.get("hometown");
+          hometownLabel = "Hometown: " + hometown;
         primaryMajor = user.get("userMajor");
+          primaryMajorLabel = "Major: " + primaryMajor;
         doubleMajor = user.get("userDoubleMajor");
+          doubleMajorLabel = "Double major: " + doubleMajor;
         primaryMinor = user.get("userMinor");
+          primaryMinorLabel = "Minor: " + primaryMinor;
         doubleMinor = user.get("userDoubleMinor");
+          doubleMinorLabel = "Double minor: " + doubleMinor;
         academicInterests = user.get("userAcademicInterests");
-        campusInvolement = user.get("userCampusInvolvement");
+          academicInterestsLabel = "Academic interests: " + academicInterests;
+        campusInvolvement = user.get("userCampusInvolvement");
+          campusInvolvementLabel = "Campus involvement: " + campusInvolvement;
         phoneNum = user.get("userPhoneNum");
+          phoneNumLabel = "Phone number: " + phoneNum;
         personalEmail = user.get("userPersonalEmail");
+          personalEmailLabel = "Personal email: " + personalEmail;
         facebook = user.get("userFacebook");
+          facebookURL = "<a href='http://facebook.com/" + facebook + "'>" + facebook + "</a>";
+          facebookURLLabel = "Facebook: " + facebookURL;
         twitter = user.get("userTwitter");
+          twitterURL = "<a href='http://twitter.com/" + twitter + "'>" + twitter + "</a>";
+          twitterURLLabel = "Twitter: " + twitterURL;
         instagram = user.get("userInstagram");
+          instagramURL = "<a href='http://instagram.com/" + instagram + "'>" + instagram + "</a>";
+          instagramURLLabel = "Instagram: " + instagramURL;
+
 
         if (doubleMajor !== undefined) {
-          document.getElementById("doubleMajor").innerHTML = doubleMajor;
+          document.getElementById("doubleMajor").innerHTML = doubleMajorLabel;
         } else { }
         if (primaryMinor !== undefined) {
-          document.getElementById("primaryMinor").innerHTML = primaryMinor;
+          document.getElementById("primaryMinor").innerHTML = primaryMinorLabel;
         } else { }
         if (doubleMinor !== undefined) {
-          document.getElementById("doubleMinor").innerHTML = doubleMinor;
+          document.getElementById("doubleMinor").innerHTML = doubleMinorLabel;
         } else { }
 
         document.getElementById("fullName").innerHTML = fullName;
         document.getElementById("bio").innerHTML = bio;
-        // document.getElementById("hometown").innerHTML = hometown;
+        document.getElementById("hometown").innerHTML = hometownLabel;
         document.getElementById("roleTeam").innerHTML = roleTeam;
-        document.getElementById("classStatus").innerHTML = classStatus;
-        document.getElementById("primaryMajor").innerHTML = primaryMajor;
-        document.getElementById("academicInterests").innerHTML = academicInterests;
-        document.getElementById("campusInvolement").innerHTML = campusInvolement;
-        document.getElementById("phoneNum").innerHTML = phoneNum;
-        document.getElementById("personalEmail").innerHTML = personalEmail;
-        document.getElementById("facebook").innerHTML = facebook;
-        document.getElementById("twitter").innerHTML = twitter;
-        document.getElementById("instagram").innerHTML = instagram;
+        document.getElementById("classStatus").innerHTML = classStatusLabel;
+        document.getElementById("primaryMajor").innerHTML = primaryMajorLabel;
+        document.getElementById("academicInterests").innerHTML = academicInterestsLabel;
+        document.getElementById("campusInvolvement").innerHTML = campusInvolvementLabel;
+        document.getElementById("phoneNum").innerHTML = phoneNumLabel;
+        document.getElementById("personalEmail").innerHTML = personalEmailLabel;
+        document.getElementById("facebook").innerHTML = facebookURLLabel;
+        document.getElementById("twitter").innerHTML = twitterURLLabel;
+        document.getElementById("instagram").innerHTML = instagramURLLabel;
 
       },
       error: function(error) {
-        supersonic.logger.info("error occurred when retrieving data");
+        supersonic.logger.info("Error occurred when retrieving data");
       }
     });
 
@@ -92,22 +122,15 @@ angular
 
     //switcher operation
     $scope.state = "ACADEMICS";
-
-    // $scope.profilePersonal = function() {
-    //   $scope.state = "PERSONAL";
-    //   supersonic.logger.info("personal");
-    // }
     $scope.profileAcademics = function() {
       $scope.state = "ACADEMICS";
       supersonic.logger.info("academics");
+      // document.getElementById('academicTab').style.color = 'red';
     }
-    // $scope.profileContact = function() {
-    //   $scope.state = "CONTACT";
-    //   supersonic.logger.info("contact");
-    // }
     $scope.profileConnect = function() {
       $scope.state = "CONNECT";
       supersonic.logger.info("connect");
+      // document.getElementById('connectTab').style.color = 'red';
     }
 
     //showing edit profile view and saving information
@@ -116,6 +139,15 @@ angular
       var view = new supersonic.ui.View("example#edit-profile");
       supersonic.ui.layers.push(view);
     }
+
+    // $scope.facebookModal = function(facebookModal) {
+    //   var modalView = new supersonic.ui.View(facebookURL);
+    //   var options = {
+    //   animate: true
+    //   }
+    //   supersonic.ui.modal.show(modalView, options);
+    // }
+
 
 
 
